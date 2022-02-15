cced5ea7823ff113986c75c7d7af8e1de40d31cd
pK <pave.kruchin@gmail.com>
2022-02-15 15:41:12 +0200

    delete storyboard

---
+++ ToDoList/ToDoList/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Pavel Olegovich on 15.02.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
